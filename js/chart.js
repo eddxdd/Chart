8257efc22235afa381a11d20b5efb824384bbcba
Eduardo Lemos <eddu1930@gmail.com>
2017-12-04 14:50:52 -0500

    Add files via upload

--- js/chart.js
+++ js/chart.js
@@ -3,14 +3,14 @@ let myChart = document.getElementById('myChart').getContext('2d');
 let mainChart = new Chart(myChart, {
   type: 'bar',
   data:{
-    labels:['London', 'Toronto', 'Branford', 'Waterloo'],
+    labels:['Toronto', 'London', 'Branford', 'Waterloo'],
     datasets:[{
       label:'Population',
       data:[
         617594,
-        391045,
-        203060,
-        256519
+        491045,
+        253060,
+        356519
       ]
     }]
   },
